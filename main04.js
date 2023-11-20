0d1bfc527fe677be41f5a67e6b44cc252a3f9161
SantiagoVallejo1 <santtyvallejo6@gmail.com>
2023-11-20 17:25:26 -0500

    Ejercicios HTML-CSS 2.0

--- main04.js
+++ main04.js
@@ -1,10 +1,17 @@
-let signUp = document.getElementById("singUp");
-let signIn = document.getElementById("singIn");
+let signUp = document.getElementById("signUp");
+let signIn = document.getElementById("signIn");
 let nameIput = document.getElementById("nameInput");
 let title = document.getElementById("title");
 
-signIn.onclick = function(){
+signUp.onclick = function(){
     nameInput.style.maxHeight = "0";
+    title.innerHTML = "Login";
+    signUp.classList.remove("disable");
+    signIn.classList.add(disable);
+}
+
+signIn.onclick = function(){
+    nameInput.style.maxHeight = "60px";
     title.innerHTML = "Registro";
     signUp.classList.remove("disable");
     signIn.classList.add(disable);
